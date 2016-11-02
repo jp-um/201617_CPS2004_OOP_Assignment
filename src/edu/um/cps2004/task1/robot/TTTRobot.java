5c8076f20b8c568898a2a15cc5a6ce1f2b5f0c80
JP <jean.p.ebejer@um.edu.mt>
2016-11-02 16:18:19 +0100

    Fixed javadoc in TTTRobot (and re-generated docs)

--- src/edu/um/cps2004/task1/robot/TTTRobot.java
+++ src/edu/um/cps2004/task1/robot/TTTRobot.java
@@ -36,9 +36,9 @@ public interface TTTRobot {
 	 * 
 	 * @param board The current game board state - empty at first.  <b>Never Null</b>
 	 * @param turn The players turn, either X or O should play on the given board. <b>Never Null</b>  
-	 * @return The position at which to play the <i>turn</i> sign (above).  Must be
+	 * @return The position at which to play the <i>turn</i> mark (2nd parameter above).  Must be
 	 * an integer from 0 to <i>board.getBoardSize() - 1</i>.  If integer is outside this
-	 * range the robot loses.  Also if the position is already taken, the robot loses.
+	 * range the robot loses.  Also if the position is already taken, the robot loses. 
 	 */
 	int play(GameBoard board, Mark turn);
 
